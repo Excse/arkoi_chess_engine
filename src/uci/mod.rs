97b92a6c626bcf3d97df290c742e1c822eb589f4
Excse <timo@DESKTOP-RLBI982>
2024-01-26 13:22:33 +0100

    Separated the tests

--- src/uci/mod.rs
+++ src/uci/mod.rs
@@ -1,4 +1,5 @@
 pub mod error;
+mod tests;
 
 use std::io::{BufRead, Write};
 
@@ -198,88 +199,3 @@ impl UCI {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use std::io::Cursor;
-
-    use crate::uci::Command;
-
-    use super::UCI;
-
-    #[test]
-    fn uci_command() {
-        let mut uci = UCI::new("ace", "Excse");
-
-        let mut reader = Cursor::new("   uci ");
-        let mut writer = Vec::<u8>::new();
-
-        let result = uci.receive_command(&mut reader, &mut writer);
-        matches!(result, Ok(Command::None));
-
-        let output = String::from_utf8(writer).unwrap();
-        let output: Vec<String> = output.lines().map(String::from).collect();
-
-        assert_eq!(output.len(), 3);
-        assert_eq!(output[0], "id name ace");
-        assert_eq!(output[1], "id author Excse");
-        assert_eq!(output[2], "uciok");
-    }
-
-    #[test]
-    fn debug_command() {
-        let mut uci = UCI::new("ace", "Excse");
-        assert_eq!(uci.debug, false);
-
-        let mut writer = Vec::<u8>::new();
-
-        let mut reader = Cursor::new("   debug ");
-        let result = uci.receive_command(&mut reader, &mut writer);
-        assert!(result.is_err());
-
-        let mut reader = Cursor::new("  debug   toggle   ");
-        let result = uci.receive_command(&mut reader, &mut writer);
-        assert!(result.is_err());
-
-        let mut reader = Cursor::new(" debug   on ");
-        let result = uci.receive_command(&mut reader, &mut writer);
-        matches!(result, Ok(Command::None));
-        assert_eq!(uci.debug, true);
-
-        let mut reader = Cursor::new("    debug   off  ");
-        let result = uci.receive_command(&mut reader, &mut writer);
-        matches!(result, Ok(Command::None));
-        assert_eq!(uci.debug, false);
-    }
-
-    #[test]
-    fn isready_command() {
-        let mut uci = UCI::new("ace", "Excse");
-        assert_eq!(uci.debug, false);
-
-        let mut reader = Cursor::new(" isready  ");
-        let mut writer = Vec::<u8>::new();
-
-        let result = uci.receive_command(&mut reader, &mut writer);
-        matches!(result, Ok(Command::IsReady));
-
-        let result = uci.send_readyok(&mut writer);
-        matches!(result, Ok(Command::None));
-
-        let output = String::from_utf8(writer).unwrap();
-        let output: Vec<String> = output.lines().map(String::from).collect();
-
-        assert_eq!(output.len(), 1);
-        assert_eq!(output[0], "readyok");
-    }
-
-    #[test]
-    fn quit_command() {
-        let mut uci = UCI::new("ace", "Excse");
-
-        let mut reader = Cursor::new("   quit ");
-        let mut writer = Vec::<u8>::new();
-
-        let result = uci.receive_command(&mut reader, &mut writer);
-        matches!(result, Ok(Command::Quit));
-    }
-}
