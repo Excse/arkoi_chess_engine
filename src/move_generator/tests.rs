742426d4465276e83476e8b69095f7ff5bae00bc
Excse <timo@DESKTOP-RLBI982>
2024-02-03 16:02:26 +0100

    Added perft stats

--- src/move_generator/tests.rs
+++ src/move_generator/tests.rs
@@ -17,8 +17,12 @@ mod perft {
         let move_generator = MoveGenerator::default();
         let result = perft(&board, &move_generator, 0).unwrap();
         assert_eq!(result.nodes, 1);
-        assert_eq!(result.castles, 0);
+        assert_eq!(result.captures, 0);
         assert_eq!(result.en_passants, 0);
+        assert_eq!(result.castles, 0);
+        assert_eq!(result.promotions, 0);
+        assert_eq!(result.checks, 0);
+        assert_eq!(result.double_checks, 0);
     }
 
     #[test]
@@ -29,8 +33,12 @@ mod perft {
         let move_generator = MoveGenerator::default();
         let result = perft(&board, &move_generator, 1).unwrap();
         assert_eq!(result.nodes, 20);
-        assert_eq!(result.castles, 0);
+        assert_eq!(result.captures, 0);
         assert_eq!(result.en_passants, 0);
+        assert_eq!(result.castles, 0);
+        assert_eq!(result.promotions, 0);
+        assert_eq!(result.checks, 0);
+        assert_eq!(result.double_checks, 0);
     }
 
     #[test]
@@ -41,8 +49,12 @@ mod perft {
         let move_generator = MoveGenerator::default();
         let result = perft(&board, &move_generator, 2).unwrap();
         assert_eq!(result.nodes, 400);
-        assert_eq!(result.castles, 0);
+        assert_eq!(result.captures, 0);
         assert_eq!(result.en_passants, 0);
+        assert_eq!(result.castles, 0);
+        assert_eq!(result.promotions, 0);
+        assert_eq!(result.checks, 0);
+        assert_eq!(result.double_checks, 0);
     }
 
     #[test]
@@ -53,8 +65,12 @@ mod perft {
         let move_generator = MoveGenerator::default();
         let result = perft(&board, &move_generator, 3).unwrap();
         assert_eq!(result.nodes, 8902);
-        assert_eq!(result.castles, 0);
+        assert_eq!(result.captures, 34);
         assert_eq!(result.en_passants, 0);
+        assert_eq!(result.castles, 0);
+        assert_eq!(result.promotions, 0);
+        assert_eq!(result.checks, 12);
+        assert_eq!(result.double_checks, 0);
     }
 
     #[test]
@@ -65,8 +81,12 @@ mod perft {
         let move_generator = MoveGenerator::default();
         let result = perft(&board, &move_generator, 4).unwrap();
         assert_eq!(result.nodes, 197281);
-        assert_eq!(result.castles, 0);
+        assert_eq!(result.captures, 1576);
         assert_eq!(result.en_passants, 0);
+        assert_eq!(result.castles, 0);
+        assert_eq!(result.promotions, 0);
+        assert_eq!(result.checks, 469);
+        assert_eq!(result.double_checks, 0);
     }
 
     #[test]
@@ -77,8 +97,44 @@ mod perft {
         let move_generator = MoveGenerator::default();
         let result = perft(&board, &move_generator, 5).unwrap();
         assert_eq!(result.nodes, 4865609);
-        assert_eq!(result.castles, 0);
+        assert_eq!(result.captures, 82719);
         assert_eq!(result.en_passants, 258);
+        assert_eq!(result.castles, 0);
+        assert_eq!(result.promotions, 0);
+        assert_eq!(result.checks, 27351);
+        assert_eq!(result.double_checks, 0);
+    }
+
+    #[test]
+    fn perft_startpos_6() {
+        let mut rand = StdRng::seed_from_u64(42);
+        let hasher = ZobristHasher::new(&mut rand);
+        let board = Board::default(&hasher);
+        let move_generator = MoveGenerator::default();
+        let result = perft(&board, &move_generator, 6).unwrap();
+        assert_eq!(result.nodes, 119060324);
+        assert_eq!(result.captures, 2812008);
+        assert_eq!(result.en_passants, 5248);
+        assert_eq!(result.castles, 0);
+        assert_eq!(result.promotions, 0);
+        assert_eq!(result.checks, 809099);
+        assert_eq!(result.double_checks, 46);
+    }
+
+    #[test]
+    fn perft_startpos_7() {
+        let mut rand = StdRng::seed_from_u64(42);
+        let hasher = ZobristHasher::new(&mut rand);
+        let board = Board::default(&hasher);
+        let move_generator = MoveGenerator::default();
+        let result = perft(&board, &move_generator, 7).unwrap();
+        assert_eq!(result.nodes, 3195901860);
+        assert_eq!(result.captures, 108329926);
+        assert_eq!(result.en_passants, 319617);
+        assert_eq!(result.castles, 883453);
+        assert_eq!(result.promotions, 0);
+        assert_eq!(result.checks, 33103848);
+        assert_eq!(result.double_checks, 1628);
     }
 
     #[test]
@@ -124,8 +180,11 @@ mod perft {
     struct PerftResult {
         nodes: usize,
         captures: usize,
-        castles: usize,
         en_passants: usize,
+        castles: usize,
+        promotions: usize,
+        checks: usize,
+        double_checks: usize,
     }
 
     impl PerftResult {
@@ -133,8 +192,11 @@ mod perft {
             Self {
                 nodes: 0,
                 captures: 0,
-                castles: 0,
                 en_passants: 0,
+                castles: 0,
+                promotions: 0,
+                checks: 0,
+                double_checks: 0,
             }
         }
     }
@@ -144,8 +206,11 @@ mod perft {
             Self {
                 nodes: 1,
                 captures: 0,
-                castles: 0,
                 en_passants: 0,
+                castles: 0,
+                promotions: 0,
+                checks: 0,
+                double_checks: 0,
             }
         }
     }
@@ -154,8 +219,11 @@ mod perft {
         fn add_assign(&mut self, rhs: Self) {
             self.nodes += rhs.nodes;
             self.captures += rhs.captures;
-            self.castles += rhs.castles;
             self.en_passants += rhs.en_passants;
+            self.castles += rhs.castles;
+            self.promotions += rhs.promotions;
+            self.checks += rhs.checks;
+            self.double_checks += rhs.double_checks;
         }
     }
 
@@ -172,14 +240,43 @@ mod perft {
 
         let moves = move_generator.get_legal_moves(board)?;
         for mov in moves {
-            match mov.kind {
-                MoveKind::Castle(_) => result.castles += 1,
-                MoveKind::EnPassant(_) => result.en_passants += 1,
-                _ => {}
-            }
-
             let mut board = board.clone();
             board.make(&mov)?;
+
+            if depth == 1 {
+                let king = board.get_king_square(board.active).unwrap();
+                let checkers = move_generator.get_checkers(&board, king).len();
+                if checkers != 0 {
+                    result.checks += 1;
+                }
+
+                if checkers == 2 {
+                    result.double_checks += 1;
+                }
+
+                // TODO: Add discovery checks and checkmate
+
+                match mov.kind {
+                    MoveKind::Castle(_) => result.castles += 1,
+                    MoveKind::EnPassant(_) => {
+                        result.en_passants += 1;
+                        result.captures += 1;
+                    }
+                    MoveKind::Attack => {
+                        result.captures += 1;
+                    }
+                    MoveKind::Promotion(ref mov) => {
+                        result.promotions += 1;
+                        if mov.is_attack {
+                            result.captures += 1;
+                        }
+                    }
+                    _ => {}
+                }
+
+                result.nodes += 1;
+                continue;
+            }
 
             let next_perft = perft(&board, move_generator, depth - 1)?;
             result += next_perft;
